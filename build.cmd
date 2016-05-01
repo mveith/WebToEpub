d13f98ba8778e6c08bbb2a7759067b83d44e2682
mveith <miroveith@windowslive.com>
2016-05-01 20:47:57 +0200

    Paket

---
+++ build.cmd
@@ -0,0 +1,13 @@
+@echo off
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+if not exist paket.lock (
+  .paket\paket.exe install
+) else (
+  .paket\paket.exe restore
+)
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
