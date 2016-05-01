b67d3c0342e739d183b01b40692872502d0b4016
mveith <miroveith@windowslive.com>
2016-05-01 21:28:42 +0200

    Empty website

---
+++ WebToEpub.Website/App.fs
@@ -0,0 +1,8 @@
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+
+let webPart = choose [ GET >=> choose [ path "/" >=> OK "Response..." ] ]
+
+startWebServer defaultConfig webPart
