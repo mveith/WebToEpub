0e4c48b4fbdf19fafabc70defd2e78139ab91fd3
mveith <miroveith@windowslive.com>
2016-05-08 00:34:55 +0200

    Simple web page converting web from imput to epub attachment (fake)

--- WebToEpub.Website/App.fs
+++ WebToEpub.Website/App.fs
@@ -3,6 +3,27 @@ open Suave.Filters
 open Suave.Operators
 open Suave.Successful
 
-let webPart = choose [ GET >=> choose [ path "/" >=> OK "Response..." ] ]
+let getIndexPage = 
+    "Index.html"
+    |> System.IO.File.ReadAllText
+    |> OK
+
+let setAttachmentHeader attachmentFileName = 
+    Suave.Writers.setHeader "Content-Disposition" ("attachment; filename=" + attachmentFileName)
+
+let downloadEpub (webPageUri : string) = 
+    let (epubName, epubBytes) = ("test.epub", System.Text.Encoding.UTF8.GetBytes webPageUri)
+    setAttachmentHeader epubName >=> ok epubBytes
+
+let convert = 
+    request (fun r -> 
+        let queryChoice = r.queryParam "webPageUri"
+        match queryChoice with
+        | Choice1Of2 webPageUri -> webPageUri |> downloadEpub
+        | Choice2Of2 msg -> Suave.RequestErrors.BAD_REQUEST(sprintf "webPageUri: %s" msg))
+
+let webPart = 
+    choose [ GET >=> choose [ path "/" >=> getIndexPage
+                              path "/convert" >=> convert ] ]
 
 startWebServer defaultConfig webPart
