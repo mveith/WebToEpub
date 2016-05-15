3373b34ec9fd75a351ae3af17b5e9855226dad83
mveith <miroveith@windowslive.com>
2016-05-15 10:34:34 +0200

    Travis - next try (FakeLib.dll path changed)

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
 mono .paket/paket.bootstrapper.exe
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
-	exit $exit_code
+    exit $exit_code
 fi
 
 mono .paket/paket.exe restore
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
-	exit $exit_code
+    exit $exit_code
 fi
-mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+
+mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx -d:MONO $@
