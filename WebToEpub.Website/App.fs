c2c70c937cb72cf829c08395c06c305bb660bf24
mveith <miroveith@windowslive.com>
2016-06-16 22:16:02 +0200

    convert with POST method, save as file and download result

--- WebToEpub.Website/App.fs
+++ WebToEpub.Website/App.fs
@@ -6,24 +6,36 @@ open Suave.Successful
 open ReadableContent
 open EpubBuilder
 open System.Net
+open System.IO
 
 let setAttachmentHeader attachmentFileName = 
     let attachmentFileName = System.Uri.EscapeDataString attachmentFileName
     Suave.Writers.setHeader "Content-Disposition" ("attachment; filename*=UTF-8''" + attachmentFileName)
 
-let downloadEpub (webPageUri : string) = 
+let saveEpub (webPageUri : string) = 
     let epub = 
         webPageUri
         |> getBookFromUrl
         |> buildEpub
-    setAttachmentHeader epub.Name >=> ok epub.Data
+    File.WriteAllBytes(epub.Name, epub.Data)
+    epub.Name
 
 let convert = 
     request (fun r -> 
         let queryChoice = r.queryParam "webPageUri"
         match queryChoice with
-        | Choice1Of2 webPageUri -> webPageUri |> downloadEpub
+        | Choice1Of2 webPageUri -> 
+            webPageUri
+            |> saveEpub
+            |> OK
         | Choice2Of2 msg -> Suave.RequestErrors.BAD_REQUEST(sprintf "webPageUri: %s" msg))
+
+let download = 
+    request (fun r -> 
+        let queryChoice = r.queryParam "epubName"
+        match queryChoice with
+        | Choice1Of2 epubName -> setAttachmentHeader epubName >=> ok (File.ReadAllBytes epubName)
+        | Choice2Of2 msg -> Suave.RequestErrors.BAD_REQUEST(sprintf "epubName: %s" msg))
 
 let canConvert = 
     request (fun r -> 
@@ -39,9 +51,10 @@ let canConvert =
 
 let webPart = 
     choose [ GET >=> choose [ path "/" >=> file "Index.html"
-                              path "/convert" >=> convert
+                              path "/download" >=> download
                               path "/canConvert" >=> canConvert
-                              path "/style.css" >=> file "style.css" ] ]
+                              path "/style.css" >=> file "style.css" ]
+             POST >=> choose [ path "/convert" >=> convert ] ]
 
 // change default bindings to avoid problems with Docker ports accesibility
 let config = { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Any 8083us ] }
